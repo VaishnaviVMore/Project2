3659a60b1bc2a3eeb1f5ae1d6cc2ede4006847b5
VaishnaviVMore <07mvaishnavi@gmail.com>
2024-09-26 23:05:27 +0530

    Max product subarray in given array

---
+++ Maxproductsubarr.java
@@ -0,0 +1,30 @@
+public class Maxproductsubarr {
+    public static int Product(int arr[]){
+        int n=arr.length;
+            int pro=1;
+            int maxpro=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int start=i;
+            for(int j=i;j<n;j++){
+                int end=j;
+                pro=1;
+                for(int k=start;k<=end;k++){
+                    pro=pro*arr[k];
+                    //System.out.println(arr[k]+" ");
+                }
+                if(pro>maxpro){
+                    maxpro=pro;
+                }
+            }
+           
+        }
+        
+        return maxpro;
+            
+    
+    }
+    public static void main(String[] args) {
+        int arr[]={1,3,5,7,9};
+        System.out.println(Product(arr));
+    }
+}
