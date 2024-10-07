a9a8b05918198774edbdae99d778b475ccc2c267
VaishnaviVMore <07mvaishnavi@gmail.com>
2024-10-07 23:07:52 +0530

    Array practice questions by Harry

---
+++ Harry1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Harry1 {
+    public static void main(String[] args) {
+        //PROBLEM1
+        // float arr[]=new float[5];
+        // Scanner Sc=new Scanner(System.in);
+        // for(int j=0;j<5;j++){
+        //     arr[j]=Sc.nextFloat();
+        // }
+        // float sum=0;
+        // for(int i=0;i<5;i++){
+        //     sum+=arr[i];
+        // }
+        // System.out.println(sum);
+        // float arr[]={2.6f,7.6f,3.9f,5.8f,5.5f};
+        // float sum=0;
+        // for(int i=0;i<arr.length;i++){
+        //     sum+=arr[i];
+        // }
+        // System.out.println(sum);
+        //PROBLEM2
+        float arr[]={3.4f,5.6f,7.8f,9.9f,5.7f};
+        float num=5.6f;
+        boolean isInarray=false;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==num){
+                isInarray=true;
+                break;
+            }
+        }
+        if(isInarray){
+            System.out.println("Number is in array");
+        }
+        else{
+            System.out.println("Number is not in array");
+        }
+    }
+}
